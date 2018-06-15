77003954f553bb5c8fe4fa2ceb9e0955e08afa09
Loïc P <loic.pirez@epitech.eu>
2018-06-15 11:16:22 +0200

    Changing exemple from GET to POST (printing POSTed data with exemple)

--- handler.js
+++ handler.js
@@ -1,10 +1,20 @@
 import message from './src/message'
 
 export const main = async (event, context, callback) => {
+  const data = JSON.parse(event.body)
+
+  if (data.message === 'undefined') {
+    return callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: 'Missing message in POST request.' })
+    })
+  }
+
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      message: `${(await message({ time: 1, copy: 'Hello world!' }))}`
+      message: `${(await message({ time: 1, copy: data.message }))}`
     })
   })
 }
